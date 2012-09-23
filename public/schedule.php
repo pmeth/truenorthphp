6349a94621a1aeb3d2c380ea4fba21e7c12d0aef
Chris Hartjes <chartjes@littlehart.net>
2012-09-23 12:02:03 -0400

    Added in stubs for the schedule page

---
+++ public/schedule.php
@@ -0,0 +1,136 @@
+<?php
+$page = 'schedule';
+?>
+<?php include '../header.php'; ?>
+      <div class="content-wrapper">
+         <div class="container">
+            <div class="row">
+               <div class="span12">
+                  <div class="page-header">
+                     <h1>Conference Schedule</h1>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="span12" >
+                <center>November 2nd, 2012</center><br>
+                  <table class="table">
+                    <tr>
+                        <th>Time</th>
+                        <th>Room 1</th>
+                        <th>Room 2</th>
+                        <th>Room 3</th>
+                    </tr>
+                    <tr>
+                        <td>9:00 to 9:50</td>
+                        <td colspan=3><center>Keynote by Reg Braithwaite</center></td>
+                    </tr>
+                    <tr>
+                        <td>10:00 to 10:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>11:00 to 11:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>12:00 to 12:50</td>
+                        <td colspan=3><center>Lunch</center></td>
+                    </tr>
+                    <tr>
+                        <td>13:00 to 13:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>14:00 to 14:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>15:00 to 15:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>16:00 to 16:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>17:00 to ?</td>
+                        <td colspan=3><center>HACKATHON</center></td>
+                    </tr>
+                  </table> 
+               </div>
+            <div class="row">
+               <div class="span12" >
+                <center>November 3rd, 2012</center><br>
+                  <table class="table">
+                    <tr>
+                        <th>Time</th>
+                        <th>Room 1</th>
+                        <th>Room 2</th>
+                        <th>Room 3</th>
+                    </tr>
+                    <tr>
+                        <td>9:00 to 9:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>10:00 to 10:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>11:00 to 11:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>12:00 to 12:50</td>
+                        <td colspan=3><center>Lunch</center></td>
+                    </tr>
+                    <tr>
+                        <td>13:00 to 13:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>14:00 to 14:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>15:00 to 15:50</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>16:00 to 16:50</td>
+                        <td colspan=3><center>Closing keynote by Chris Hartjes</center></td>
+                    </tr>
+                  </table> 
+               </div>
+            </div>
+            <hr>
+         </div>
+      </div>
+<?php include '../footer.php'; ?>
+   </body>
+</html>
