d696b002e7c962c0d0d99cec266c5ab754fc5b23
Peter Meth <pmeth@softersoftware.com>
2012-09-28 00:11:11 -0300

    Update speaker_data/Chris_Cornutt.php
    removing newlines from Chris Cornutt

--- speaker_data/Chris_Cornutt.php
+++ speaker_data/Chris_Cornutt.php
@@ -10,10 +10,7 @@ $speakers[] = array (
   'talks' => array (
     array (
       'title' => 'A (PHP) Security State of Mind',
-      'text' => 'There\'s no doubt about it, you can\'t afford to ignore security in your applications these days. With 
-        things like SQL injection and CSRF becoming more and more common, you need the knowledge to keep them from happening 
-        in your apps. It\'s not just backend either - there\'s frontend concerns too! I\'ll share with you some of the most 
-        common security issues that you need to consider and how to avoid them and harden your apps.',
+      'text' => 'There\'s no doubt about it, you can\'t afford to ignore security in your applications these days. With things like SQL injection and CSRF becoming more and more common, you need the knowledge to keep them from happening in your apps. It\'s not just backend either - there\'s frontend concerns too! I\'ll share with you some of the most common security issues that you need to consider and how to avoid them and harden your apps.',
     ),
   ),
 );
