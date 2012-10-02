aa3caccc91eab80625168a8ead867ae1c442bbcc
Peter Meth <pmeth@softersoftware.com>
2012-10-01 23:19:25 -0400

    adding the completed schedule. the tables are done as a separate file because I used a generator to make the html

---
+++ schedule.php
@@ -0,0 +1,278 @@
+            <div class="day_number">Day #1 - Friday, November 2, 2012</div>
+            <table class="table schedule_day">
+               <thead>
+                  <tr>
+                     <th>Time</th>
+                     <th>Room 1</th>
+                     <th>Room 2</th>
+                     <th>Room 3</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr class="schedule_slot">
+                     <th>09:00</th>
+                     <td class="error" colspan="3">
+                        <div class="talk_title">Opening Keynote</div>
+                        <div class="talk_speaker">Reginald Braithwaite</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>10:00</th>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=javascript_for_php_developers">Javascript for PHP Developers</a>
+                        </div>
+                        <div class="talk_speaker">Alfred Ayache</div>
+                     </td>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=avoiding_the_owasp_top____security_exploits">Avoiding the OWASP Top 10 security exploits</a>
+                        </div>
+                        <div class="talk_speaker">Mark Story</div>
+                     </td>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=introduction_to_continuous_integration_with_jenkins">Introduction to Continuous Integration with Jenkins</a>
+                        </div>
+                        <div class="talk_speaker">Eric Hogue</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>11:00</th>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=cryptography_for_the_average_developer">Cryptography For The Average Developer</a>
+                        </div>
+                        <div class="talk_speaker">Anthony Ferrara</div>
+                     </td>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=paradigm_shift__an_introduction_to_zf__for_zf__developers">Paradigm Shift: An Introduction to ZF2 for ZF1 developers</a>
+                        </div>
+                        <div class="talk_speaker">Adam Lundrigan</div>
+                     </td>
+                     <td class="info">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=growing_spiders_to_crawl_the_web">Growing spiders to crawl the web</a>
+                        </div>
+                        <div class="talk_speaker">Juozas Joe Kaziukenas</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot">
+                     <th>12:00</th>
+                     <td colspan="3"><div class="talk_title">Lunch</div></td>
+                  </tr>
+                  <tr class="schedule_slot"><th>13:00</th>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=caching_with_memcached">Caching with Memcached</a>
+                        </div>
+                        <div class="talk_speaker">Ilia Alshanetsky</div>
+                     </td>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=your_code_sucks__let_s_fix_it_">Your code sucks, let's fix it.</a>
+                        </div>
+                        <div class="talk_speaker">Rafael Dohms</div>
+                     </td>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=building_apis_and_applications_with_silex">Building APIs and Applications with Silex</a>
+                        </div>
+                        <div class="talk_speaker">Cal Evans</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>14:00</th>
+                     <td class="info">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=introduction_to_the_yii_framework__version___">Introduction to the Yii Framework (version 2)</a>
+                        </div>
+                        <div class="talk_speaker">Larry Ullman</div>
+                     </td>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=bring_your_site_to_life_using_websockets">Bring your site to life using WebSockets</a>
+                        </div>
+                        <div class="talk_speaker">Chris Boden</div>
+                     </td>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=conference_speaking____">Conference Speaking 101</a>
+                        </div>
+                        <div class="talk_speaker">Stefan Koopmanschap</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>15:00</th>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=building_php_applications_on_windows_azure">Building PHP Applications on Windows Azure</a>
+                        </div>
+                        <div class="talk_speaker">Cory Fowler</div>
+                     </td>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=javascript_apis_in_the_html__umbrella">Javascript APIs in the HTML5 Umbrella</a>
+                        </div>
+                        <div class="talk_speaker">Jake Smith</div>
+                     </td>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=the_hidden_challenges_of_developing_e_commerce">The Hidden Challenges of Developing E-Commerce</a>
+                        </div>
+                        <div class="talk_speaker">Brett Florio</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>16:00</th>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=orms_don_t_kill_your_database__developers_do_">ORMs don't kill your database, developers do!</a>
+                        </div>
+                        <div class="talk_speaker">Guilherme Blanco</div>
+                     </td>
+                     <td class="info">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=don_t_reinvent_the_wheel_with_symfony__components_">Don't Reinvent the Wheel with Symfony2 Components!</a>
+                        </div>
+                        <div class="talk_speaker">Hugo Hamon</div>
+                     </td>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=webdriver_">WebDriver!</a>
+                        </div>
+                        <div class="talk_speaker">Adam Goucher</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot">
+                     <th>18:00<br>-<br>22:00</th>
+                     <td class="warning" colspan="3"><div class="talk_title">Hackathon</div></td>
+                  </tr>
+               </tbody>
+			</table>
+
+            <div class="day_number">Day #2 - Saturday, November 3, 2012</div>
+            <table class="table schedule_day">
+               <thead>
+				  <tr>
+                     <th>Time</th>
+                     <th>Room 1</th>
+                     <th>Room 2</th>
+                     <th>Room 3</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr class="schedule_slot"><th>09:00</th>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=process_any_amounts_of_data__any_time">Process any amounts of data. Any time</a>
+                        </div>
+                        <div class="talk_speaker">Juozas Joe Kaziukenas</div>
+                     </td>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=getting_acquainted_with_mongodb">Getting Acquainted with MongoDB</a>
+                        </div>
+                        <div class="talk_speaker">Jeremy Mikola</div>
+                     </td>
+                     <td class="info">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=there_is_a_php_library_for_that_">There is a PHP Library for That!</a>
+                        </div>
+                        <div class="talk_speaker">Hugo Hamon</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>10:00</th>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=annotating_with_annotations">Annotating with Annotations</a>
+                        </div>
+                        <div class="talk_speaker">Rafael Dohms</div>
+                     </td>
+                     <td class="info">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=introduction_to_twig">Introduction to Twig</a>
+                        </div>
+                        <div class="talk_speaker">Mark Story</div>
+                     </td>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=a__php__security_state_of_mind">A (PHP) Security State of Mind</a>
+                        </div>
+                        <div class="talk_speaker">Chris Cornutt</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>11:00</th>
+                     <td class="info">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=build_more_with_less_css">Build more with LESS CSS</a>
+                        </div>
+                        <div class="talk_speaker">Jake Smith</div>
+                     </td>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=deploying_php_enterprise_apps_to_windows_azure">Deploying PHP enterprise apps to Windows Azure</a>
+                        </div>
+                        <div class="talk_speaker">Michelangelo van Dam</div>
+                     </td>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=amazon_web_services_at_scale">Amazon Web Services at Scale</a>
+                        </div>
+                        <div class="talk_speaker">Rich Poirier</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot">
+                     <th>12:00</th>
+                     <td colspan="3"><div class="talk_title">Lunch</div></td>
+                  </tr>
+                  <tr class="schedule_slot"><th>13:00</th>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=don_t_use_a_screw_when_you_need_a_nail">Don't use a screw when you need a nail</a>
+                        </div>
+                        <div class="talk_speaker">Stefan Koopmanschap</div>
+                     </td>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=recursive_software_development_teams">Recursive Software Development Teams</a>
+                        </div>
+                        <div class="talk_speaker">Scott Mattocks</div>
+                     </td>
+                     <td class="info" rowspan="3">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=unconference">Unconference</a>
+                        </div>
+                        <div class="talk_speaker"></div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>14:00</th>
+                     <td class="success">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=don_t_be_stupid__grasp_solid">Don't Be STUPID, GRASP SOLID</a>
+                        </div>
+                        <div class="talk_speaker">Anthony Ferrara</div>
+                     </td>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=practical_solr">Practical Solr</a>
+                        </div>
+                        <div class="talk_speaker">Radek Zajkowski</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot"><th>15:00</th>
+                     <td class="error">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=bottleneck_analysis">Bottleneck Analysis</a>
+                        </div>
+                        <div class="talk_speaker">Ilia Alshanetsky</div>
+                     </td>
+                     <td class="warning">
+                        <div class="talk_title">
+                           <a href="viewtalk.php?name=migrating_lamp_to_a_distributed_environment">Migrating LAMP to a distributed environment</a>
+                        </div>
+                        <div class="talk_speaker">Paul Bogdashkin</div>
+                     </td>
+                  </tr>
+                  <tr class="schedule_slot">
+                     <th>16:00</th>
+                     <td class="success" colspan="3"><div class="talk_title">Closing Keynote</div><div class="talk_speaker">Chris Hartjes</div></td>
+                  </tr>
+               </tbody>
+           </table>
