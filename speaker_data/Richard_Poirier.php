0796f09129d09369a8c98f228d153e2c5fb73e39
Peter Meth <pmeth@softersoftware.com>
2012-10-02 15:04:47 -0300

    Update speaker_data/Richard_Poirier.php

--- speaker_data/Richard_Poirier.php
+++ speaker_data/Richard_Poirier.php
@@ -4,7 +4,7 @@ $speakers[] = array (
   'name' => 'Richard Poirier',
   'city' => 'Toronto',
   'country' => 'Canada',
-  'twitter' => '',
+  'twitter' => 'richpoirier',
   'image' => 'img/Richard_Poirier.jpg',
   'bio' => 'Rich is the lead developer at <a href="http://www.wattpad.com">Wattpad</a>, where he learned (and will always be learning) how to build web applications at high scale. He\'s been writing PHP for 3 years and was a Rails developer before that. He is passionate about software development and hopes he\'s still building things with code when he\'s 80.',
   'talks' => array (
