b20dbe2d73f3149d4292bdca89443dc22597ec4d
Peter Meth <pmeth@softersoftware.com>
2012-10-08 14:25:40 -0400

    adding Laura Beth Denker to schedule

---
+++ speaker_data/Laura_Beth_Denker.php
@@ -0,0 +1,16 @@
+<?php
+
+$speakers[] = array (
+  'name' => 'Laura Beth Denker',
+  'city' => 'New York, NY',
+  'country' => 'USA',
+  'twitter' => 'elblinkin',
+  'image' => 'http://www.gravatar.com/avatar/1768a070d699cabb1b18a9f5029e385a?s=200&d=http://truenorthphp.ca/img/no-pic.jpg',
+  'bio' => '',
+  'talks' => array (
+    array (
+      'title' => 'Testing and Devops Culture',
+      'text' => '',
+    ),
+  ),
+);
