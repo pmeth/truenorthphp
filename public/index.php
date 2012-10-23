27a8466b5ec3d4aee763087fc90f0a9898da29e4
Peter Meth <pmeth@softersoftware.com>
2012-10-23 01:27:00 -0300

    adding facebook like box to homepage

--- public/index.php
+++ public/index.php
@@ -43,11 +43,23 @@ $extra_css = array('css/index.css');
                </div>
                <div class="span4">
                   <div id="twitter2" class="twitter"></div>
+                  <div class="fb-like-box" data-href="http://www.facebook.com/pages/True-North-PHP-Conference/354963517930838?ref=ts&amp;fref=ts" data-width="300" data-show-faces="true" data-stream="true" data-header="true"></div>
                </div>
             </div>
          </div>
       </div>
 <?php include '../footer.php'; ?>
+<div id="fb-root"></div>
+      <!-- start: for facebook like box -->
+      <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=11696639907";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+      <!-- end: for facebook like box -->
+
       <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
       <script type="text/javascript" src="http://malsup.github.com/jquery.corner.js"></script>
       <script type="text/javascript" src="http://malsup.github.com/chili-1.7.pack.js"></script>
