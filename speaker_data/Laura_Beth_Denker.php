b272fd25329b537f3ebaddb11c25adada3d849e0
Laura Beth Lincoln <llincoln@etsy.com>
2012-11-02 02:08:17 -0400

    LB Denker speaker details.

--- speaker_data/Laura_Beth_Denker.php
+++ speaker_data/Laura_Beth_Denker.php
@@ -5,12 +5,12 @@ $speakers[] = array (
   'city' => 'New York, NY',
   'country' => 'USA',
   'twitter' => 'elblinkin',
-  'image' => 'http://www.gravatar.com/avatar/1768a070d699cabb1b18a9f5029e385a?s=200&d=http://truenorthphp.ca/img/no-pic.jpg',
-  'bio' => '',
+  'image' => 'http://www.gravatar.com/avatar/34d6c586457e85a8155b0a26dbe4f7a0?s=200&d=http://truenorthphp.ca/img/no-pic.jpg',
+  'bio' => 'Laura Beth Denker graduated from RIT with a BS/MS in Computational Mathematics/Computer Science. She then moved on to a small job in a small city in central New York before being courted by Google, bringing her to the Big Apple, where she worked in bot test and development for over 4 years. A couple years ago she took a turn for the handmade, by helping Etsy grow its spectacular development culture and continuous deployment process as the self-proclaimed Anthropologist of Developer Culture. Now she has begun digging deeper into the stack, as an Operations Engineer.',
   'talks' => array (
     array (
       'title' => 'Testing and Devops Culture',
-      'text' => '',
+      'text' => 'Nearly every "Best Practice" was developed to address a particular situation, a specific set of problems. Most of us at some point will find ourselves, or others, blindly following some regime that just doesn't quite make sense.  Let us talk about some of these latest practices. Rediscover where they originated. The goals that they were meant to achieve.  Let us begin to separate the kernel of truth from the jargon,  and maybe learn how to identify and implement practices that fit and benefit the roles a relationships of your team or organization.'
     ),
   ),
 );
