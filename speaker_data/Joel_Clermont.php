8c5a90fe27ee6e336a545f3d04a9b10a289ae9a9
Joel Clermont <jclermont@gmail.com>
2012-11-02 08:53:54 -0400

    fixed talk title linkage

--- speaker_data/Joel_Clermont.php
+++ speaker_data/Joel_Clermont.php
@@ -9,7 +9,7 @@ $speakers[] = array (
   'bio' => 'PHP, Ruby and C# developer living in the Milwaukee area. Organizer of Milwaukee PHP user group. Addicted to learning, teaching and growing the developer community.',
   'talks' => array (
     array (
-      'title' => 'REST APIs: The "R" is not for religious',
+      'title' => 'Hypermedia APIs',
       'text' => 'REST is certainly a loaded topic. There\'s the "REST" implemented by 95% of developers building APIs (including Twitter, Facebook, Google and many others). Then there\'s the "REST" preached from on high by disciples of Lord Fielding himself.
                 <br /><br />I will talk about REST in its pure and ideal form, but without the fierce dogmatism that usually accompanies such a talk. You\'ll see the ideal and the benefits it can bring, but we\'ll bring it back down to earth and get pragmatic because we have real work to do as well. A major part of the discussion will also be why you should be building an API in the first place. Even if you never expose this API to the public, it can be a solid foundation for your own applications.',
     ),
