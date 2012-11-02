0b0449675a6ec50b985c05dd4816646f60030cd3
Peter Meth <pmeth@rogers.com>
2012-11-02 06:03:54 -0400

    fixing an unescaped apostrophe

--- speaker_data/Laura_Beth_Denker.php
+++ speaker_data/Laura_Beth_Denker.php
@@ -10,7 +10,7 @@ $speakers[] = array (
   'talks' => array (
     array (
       'title' => 'Testing and Devops Culture',
-      'text' => 'Nearly every "Best Practice" was developed to address a particular situation, a specific set of problems. Most of us at some point will find ourselves, or others, blindly following some regime that just doesn't quite make sense.  Let us talk about some of these latest practices. Rediscover where they originated. The goals that they were meant to achieve.  Let us begin to separate the kernel of truth from the jargon,  and maybe learn how to identify and implement practices that fit and benefit the roles a relationships of your team or organization.'
+      'text' => 'Nearly every "Best Practice" was developed to address a particular situation, a specific set of problems. Most of us at some point will find ourselves, or others, blindly following some regime that just doesn\'t quite make sense.  Let us talk about some of these latest practices. Rediscover where they originated. The goals that they were meant to achieve.  Let us begin to separate the kernel of truth from the jargon,  and maybe learn how to identify and implement practices that fit and benefit the roles a relationships of your team or organization.'
     ),
   ),
 );
