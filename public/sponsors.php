a7994142bef30ea58e7062e16a404187de3931b6
Peter Meth <pmeth@rogers.com>
2013-05-25 14:22:45 -0400

    Clearing the sponsor page

--- public/sponsors.php
+++ public/sponsors.php
@@ -36,38 +36,26 @@ $page = 'sponsors';
                   <h3>Gold Level</h3>
                </div>
                <div class="thumbnail span3">
-                  <a href="http://www.webnotwar.ca/"><img alt="Make Web Not War" src="http://www.webnotwar.ca/wp-content/themes/webnotwar2011/images/logo.png" width="220" height="145"></a>
+                  <a href="http://www.example.com/"><img alt="Company Name" src="http://www.dummyimage.com/220x145.png" width="220" height="145"></a>
                </div>
                <div class="span8">
-                  Make Web Not War is a passionate community of tech enthusiasts. This group originally started as an event
-                  organized by Microsoft Canada and has grown into a diverse community of developers, technologists, business
-                  owners &amp; students across Canada. We welcome anyone who is excited about the latest trends in web, mobile,
-                  cloud and open data. Whether you like .Net, PHP, Python, Java, Ruby, use open source or commercial software
-                  (or both) Make Web Not War is about building technology solutions that work together.
+                  Coming soon...
+							 </div>
+            </div>
+            <div class="row" style="margin-top: 30px;">
+               <div class="thumbnail span3">
+                  <a href="http://www.exmplae.com/"><img alt="Company" src="http://www.dummyimage.com/210x94.png" width="210" height="94"></a>
+               </div>
+               <div class="span8">
+                  Coming soon...
                </div>
             </div>
             <div class="row" style="margin-top: 30px;">
                <div class="thumbnail span3">
-                  <a href="http://www.centah.com/"><img alt="Centah" src="img/centah_logo.jpg" width="210" height="94"></a>
+                  <a href="http://www.truenorthphp.com/"><img alt="Le Company" src="http://www.dummyimage.com/210x40.png" width="210" height="40"></a>
                </div>
                <div class="span8">
-                  Centah is a collaborative, web-based work-flow management solution that connects and empowers the home services industry.
-               </div>
-            </div>
-            <div class="row" style="margin-top: 30px;">
-               <div class="thumbnail span3">
-                  <a href="http://www.eventmobi.com/"><img alt="EventMobi" src="img/eventmobi_logo.png" width="210" height="40"></a>
-               </div>
-               <div class="span8">
-                  The internet is being reborn on mobile and we at EventMobi are using it to revolutionize the events industry.
-                  EventMobi is the leading mobile platform for the largest and coolest shows and conferences. Clients like Disney
-                  and Intel have used EventMobi to disrupt and enhance the attendee experience at their events. Our mobile event
-                  guides, interactive mapping, real­time audience response and the first ever social / gaming layer customized for
-                  conferences and tradeshows have been used by over 900 events, 2 Million users, and are served in 6 languages
-                  across 5 continents.<br>
-                  <br>
-                  We are also <a href="http://www.eventmobi.com/about/careers/">hiring PHP developers</a> to push the boundaries
-                  of the mobile web!
+                  Coming soon...
                </div>
             </div>
             <div class="row">
@@ -78,32 +66,25 @@ $page = 'sponsors';
                   <h3>Silver Level</h3>
                </div>
                <div class="thumbnail span3">
-                  <a href="http://www.foxycart.com/"><img alt="FoxyCart: Ecommerce for Developers" src="img/foxycart-210x105.png" width="210" height="105" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x105.png" width="210" height="105" /></a>
                </div>
                <div class="span8">
-                  <p><strong>Helping developers</strong> create flexible, powerful, custom ecommerce in less time, while <strong>equipping merchants</strong> with the fastest checkout flow available to their customers.</p>
-                  <p>FoxyCart is e-commerce done differently. We're deliberately not an all-in-one, turnkey solution, but rather the best tool for the job, complimenting the other tools already in your toolbox. We've done over $100,000,000 in sales through our system for thousands of merchants around the globe (including Canada!), and we're looking forward to saying hello at True North PHP 2012!</p>
-                  <p>&nbsp;</p>
+                  Coming soon...
                </div>
                <br>
                <div class="thumbnail span3">
-                  <a href="http://www.gsn.com/"><img alt="GSN Digital" src="img/gsn-games.jpg" width="210" height="210" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x210.png" width="210" height="210" /></a>
                </div>
                <div class="span8">
-                  GSN Digital is the interactive division of GSN (Game Show Network), which distributes game show programming to 75 million households. We offer the Web's most engaging games, sweepstakes and contests, managing more than $175 million dollars in prizing through our destination sites GSN.com and WorldWinner.com. Our mobile games offering extends the adrenaline-fueled experience, bringing the GSN Casino to gamers on the go. Our social games team powers the Games by GSN app, the largest multi-game portal on Facebook, offering free games, tournaments and a virtual online casino enjoyed by 5 million monthly active users. GSN and its subsidiary, WorldWinner.com, Inc., are owned by DIRECTV and Sony Pictures Entertainment. For further information, visit GSN at www.gsn.com. <a href="http://www.gsngames.com">We're hiring</a>!
+                  Coming soon...
                </div>
             </div>
             <div class="row" style="margin-top: 30px;">
                <div class="thumbnail span3">
-                  <a href="http://www.wattpad.com/"><img alt="Wattpad" src="img/wattpad_logo.png" width="210" height="61" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x61.png" width="210" height="61" /></a>
                </div>
                <div class="span8">
-                  <p>
-                     Wattpad is the world's largest community of readers and writers. Every day millions of people discover great fiction on Wattpad and share their own creative writing spanning mystery, romance, sci-fi, poetry, fan fiction and more. Wattpad's mobile-first model of storytelling brings "web speed" to the publishing industry to remove barriers and build communities around content. Wattpad stories are social at their core, free, collaborative, and convenient - available on the devices people already have in their pockets.
-                  </p>
-                  <p>
-                     Funded by top venture capital firms in New York and San Francisco, Wattpad is changing the way people discover and share stories. Want to join our team? <a href="http://www.wattpad.com/jobs">www.wattpad.com/jobs</a>
-                  </p>
+                  Coming soon...
                </div>
             </div>
             <div class="row">
@@ -116,30 +97,28 @@ $page = 'sponsors';
             </div>
             <div class="row">
                <div class="thumbnail span3">
-                  <a href="http://www.freshbooks.com/careers/senior-front-end-developer.php"><img alt="Freshbooks" src="img/freshbooks-logo.png" width="210" height="109"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x109.png" width="210" height="109" /></a>
                </div>
                <div class="span8">
-                  FreshBooks: Cloud Accounting. We're hiring experienced PHP developers!
+                  Coming soon...
                 </div>
             </div>
             <br>
             <div class="row">
                <div class="thumbnail span3">
-                  <a href="http://servergrove.com/"><img alt="Servergrove" src="http://servergrove.com/images/logos/sg230x35_p.png" style="margin: 10px 0;" width="220" height="33"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/220x33.png" width="220" height="33" /></a>
                </div>
                <div class="span8">
-                  ServerGrove provides hosting solutions for sites and web applications built using PHP, PHP Frameworks and MongoDB. Since its inception in 2005, ServerGrove has been dedicated to the advancement of PHP having backed countless conferences and meetups and contributed code and resources to open-source projects.  ServerGrove is one of the few hosting companies in the world that can boast of a state of the art infrastructure sitting atop of one of the most connected points in the globe, with a staff dedicated to providing support for PHP software and a solid commitment to the PHP community.
+                  Coming soon...
                 </div>
             </div>
             <br>
             <div class="row">
                <div class="thumbnail span3">
-                  <a href="http://www.10gen.com/"><img alt="10gen" src="img/10gen-logo.png" width="211" height="56"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/211x56.png" width="211" height="56" /></a>
                </div>
                <div class="span8">
-                  10gen is the company behind MongoDB, leading development, building community, providing consulting, support,
-                  and training, and offering MongoDB Management Service (MMS). MongoDB is an open source document-oriented
-                  database helping users quickly and easily deliver, scale, and operate applications.
+                  Coming soon...
                 </div>
             </div>
             <div class="row">
@@ -150,28 +129,28 @@ $page = 'sponsors';
                   <h3>Community Level</h3>
                </div>
                <div class="span2">
-                  <a href="http://meetup.gtaphp.org"><img alt="GTA PHP User Group" style="border:1px solid black" src="http://photos3.meetupstatic.com/photos/event/8/d/0/4/highres_14496100.jpeg" width="120" height="120"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/120x120.png" width="120" height="120" /></a>
                </div>
                <div class="span2">
-                  <a href="http://www.mkepug.org"><img alt="Milwaukee PHP Users Group" style="border:1px solid black" src="/img/mkepug-logo.jpg" width="110" height="165"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/110x165.png" width="110" height="165" /></a>
                </div>
                <div class="span2">
-                  <a href="http://www.nashvillephp.org"><img alt="Nashville PHP" style="border:1px solid black" src="/img/nashvillephp_logo.jpg" width="165" height="165"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/165x165.png" width="165" height="165" /></a>
                </div>
                <div class="span3">
-									<a href="http://www.atlantaphp.org"><img alt="Atlanta PHP" src="/img/atlantaphp_logo.png" width="210" height="73"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x73.png" width="210" height="73" /></a>
                   <br>
                   <br>
                   <br>
-                  <a href="http://www.homezilla.ca"><img alt="Homezilla" src="/img/homezilla_logo.jpg" width="210" height="51"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x51.png" width="210" height="51"></a>
                </div>
                <div class="span3">
-                  <a href="http://www.meetup.com/dallasphp/"><img alt="Dallas PHP" src="http://photos3.meetupstatic.com/photos/event/8/f/7/a/global_10056730.jpeg" width="180" height="180"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/180x180.png" width="180" height="180"></a>
                </div>
             </div>
             <div class="row">
                <div class="span3">
-                  <a href="http://nashville.coderfaire.com"><img alt="CoderFaire" src="http://nashville.coderfaire.com/wp-content/themes/coderfaire/images/logo.png" width="205" height="124"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/205x124.png" width="205" height="124"></a>
                </div>
             </div>
          </div>
