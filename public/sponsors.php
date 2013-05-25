d34c3a0e7218771821cd9692f2656bdad2695fc4
Peter Meth <pmeth@rogers.com>
2013-05-25 14:33:32 -0400

    clearing sponsor page and filling in with dummy images

--- public/sponsors.php
+++ public/sponsors.php
@@ -36,7 +36,7 @@ $page = 'sponsors';
                   <h3>Gold Level</h3>
                </div>
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="Company Name" src="http://www.dummyimage.com/220x145.png" width="220" height="145"></a>
+                  <a href="http://www.example.com/"><img alt="Company Name" src="http://www.dummyimage.com/220x145/000/fff&text=Your+Name+Here" width="220" height="145"></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -44,7 +44,7 @@ $page = 'sponsors';
             </div>
             <div class="row" style="margin-top: 30px;">
                <div class="thumbnail span3">
-                  <a href="http://www.exmplae.com/"><img alt="Company" src="http://www.dummyimage.com/210x94.png" width="210" height="94"></a>
+                  <a href="http://www.example.com/"><img alt="Company" src="http://www.dummyimage.com/210x94/000/fff&text=Your+Name+Here" width="210" height="94"></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -52,7 +52,7 @@ $page = 'sponsors';
             </div>
             <div class="row" style="margin-top: 30px;">
                <div class="thumbnail span3">
-                  <a href="http://www.truenorthphp.com/"><img alt="Le Company" src="http://www.dummyimage.com/210x40.png" width="210" height="40"></a>
+                  <a href="http://www.truenorthphp.com/"><img alt="Le Company" src="http://www.dummyimage.com/210x40/000/fff&text=Your+Name+Here" width="210" height="40"></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -66,14 +66,7 @@ $page = 'sponsors';
                   <h3>Silver Level</h3>
                </div>
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x105.png" width="210" height="105" /></a>
-               </div>
-               <div class="span8">
-                  Coming soon...
-               </div>
-               <br>
-               <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x210.png" width="210" height="210" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x105/000/fff&text=Your+Name+Here" width="210" height="105" /></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -81,7 +74,15 @@ $page = 'sponsors';
             </div>
             <div class="row" style="margin-top: 30px;">
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x61.png" width="210" height="61" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x210/000/fff&text=Your+Name+Here" width="210" height="210" /></a>
+               </div>
+               <div class="span8">
+                  Coming soon...
+               </div>
+            </div>
+            <div class="row" style="margin-top: 30px;">
+               <div class="thumbnail span3">
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x61/000/fff&text=Your+Name+Here" width="210" height="61" /></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -97,7 +98,7 @@ $page = 'sponsors';
             </div>
             <div class="row">
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x109.png" width="210" height="109" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x109/000/fff&text=Your+Name+Here" width="210" height="109" /></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -106,7 +107,7 @@ $page = 'sponsors';
             <br>
             <div class="row">
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/220x33.png" width="220" height="33" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/220x33/000/fff&text=Your+Name+Here" width="220" height="33" /></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -115,7 +116,7 @@ $page = 'sponsors';
             <br>
             <div class="row">
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/211x56.png" width="211" height="56" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/211x56/000/fff&text=Your+Name+Here" width="211" height="56" /></a>
                </div>
                <div class="span8">
                   Coming soon...
@@ -129,28 +130,28 @@ $page = 'sponsors';
                   <h3>Community Level</h3>
                </div>
                <div class="span2">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/120x120.png" width="120" height="120" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/120x120/000/fff&text=Your+Name+Here" width="120" height="120" /></a>
                </div>
                <div class="span2">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/110x165.png" width="110" height="165" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/110x165/000/fff&text=Your+Name+Here" width="110" height="165" /></a>
                </div>
                <div class="span2">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/165x165.png" width="165" height="165" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/165x165/000/fff&text=Your+Name+Here" width="165" height="165" /></a>
                </div>
                <div class="span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x73.png" width="210" height="73" /></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x73/000/fff&text=Your+Name+Here" width="210" height="73" /></a>
                   <br>
                   <br>
                   <br>
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x51.png" width="210" height="51"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x51/000/fff&text=Your+Name+Here" width="210" height="51"></a>
                </div>
                <div class="span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/180x180.png" width="180" height="180"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/180x180/000/fff&text=Your+Name+Here" width="180" height="180"></a>
                </div>
             </div>
             <div class="row">
                <div class="span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/205x124.png" width="205" height="124"></a>
+                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/205x124/000/fff&text=Your+Name+Here" width="205" height="124"></a>
                </div>
             </div>
          </div>
