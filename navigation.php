bbc803c424d2c901d7584879c099f485b4c15566
Peter Meth <pmeth@rogers.com>
2013-05-26 13:39:30 -0400

    removing schedule and speaker links, as they are empty

--- navigation.php
+++ navigation.php
@@ -7,12 +7,6 @@
                <li class="<?php echo $page == 'venue' ? 'active' : ''; ?>">
                   <a href="venue.php">Venue</a>
                </li>
-               <li class="<?php echo $page == 'schedule' ? 'active' : ''; ?>">
-                  <a href="schedule.php">Schedule</a>
-               </li>
-               <li class="<?php echo $page == 'speakers' ? 'active' : ''; ?>">
-                  <a href="speakers.php">Speakers</a>
-               </li>
                <li class="<?php echo $page == 'sponsors' ? 'active' : ''; ?>">
                   <a href="sponsors.php">Sponsors</a>
                </li>
