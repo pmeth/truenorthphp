86cfdc1a464a320c9b6f11d1c50db2466479d71a
Peter Meth <pmeth@rogers.com>
2013-07-25 22:49:52 -0400

    adding sunshine php and gurulink to sponsors page

--- public/sponsors.php
+++ public/sponsors.php
@@ -39,8 +39,8 @@ $page = 'sponsors';
                   <a href="http://www.github.com/"><img alt="Github" src="img/github-logo.png" width="220" height="58"></a>
                </div>
                <div class="span8">
-                  <b>GitHub is the best place to share code<b> with friends, co-workers, classmates, and complete strangers. Over three million people use GitHub to build amazing things together.
-							 </div>
+                  <b><a href="http://www.github.com/">GitHub</a> is the best place to share code<b> with friends, co-workers, classmates, and complete strangers. Over three million people use GitHub to build amazing things together.
+                </div>
             </div>
             <div class="row">
                <hr>
@@ -58,12 +58,28 @@ $page = 'sponsors';
                   <h3>Bronze Level</h3>
                </div>
             </div>
-            <div class="row">
+            <div class="row" style="margin-bottom: 20px">
                <div class="thumbnail span3">
                   <a href="http://servergrove.com/"><img alt="Servergrove" src="http://servergrove.com/images/logos/sg230x35_p.png" style="margin: 10px 0;" width="220" height="33"></a>
                </div>
                <div class="span8">
-                  ServerGrove provides hosting solutions for sites and web applications built using PHP, PHP Frameworks and MongoDB. Since its inception in 2005, ServerGrove has been dedicated to the advancement of PHP having backed countless conferences and meetups and contributed code and resources to open-source projects.  ServerGrove is one of the few hosting companies in the world that can boast of a state of the art infrastructure sitting atop of one of the most connected points in the globe, with a staff dedicated to providing support for PHP software and a solid commitment to the PHP community.
+                  <a href="http://servergrove.com/">ServerGrove</a> provides hosting solutions for sites and web applications built using PHP, PHP Frameworks and MongoDB. Since its inception in 2005, ServerGrove has been dedicated to the advancement of PHP having backed countless conferences and meetups and contributed code and resources to open-source projects.  ServerGrove is one of the few hosting companies in the world that can boast of a state of the art infrastructure sitting atop of one of the most connected points in the globe, with a staff dedicated to providing support for PHP software and a solid commitment to the PHP community.
+                </div>
+            </div>
+            <div class="row" style="margin-bottom: 20px">
+                <div class="thumbnail span3">
+                    <a href="http://sunshinephp.com/"><img alt="Sunshine PHP" src="img/sunshinephp_logo_2014.png" style="margin: 10px 0;" width="220" height="78"></a>
+                </div>
+                <div class="span8">
+                    In February it's cold in most areas of the world, but it's always warm and sunny in Florida.  Take a break from the freezing snow and enjoy a bit of warm sunshine, while talking about PHP in Miami, Florida this coming February 2014.  This year we will feature a full day of tutorials and 2 additional days of awesome talk topics, and to top it off one of the three tracks will be dedicated to PHP frameworks.  That's 10 talks specific to frameworks and 20 talks on other areas relevant to PHP.  So please come join us at <a href="http://sunshinephp.com/">http://sunshinephp.com</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="thumbnail span3">
+                    <a href="http://gurulink.ca/"><img alt="GuruLink" src="http://gurulink.ca/_include/images/logo.png" style="margin: 10px 0;" width="148" height="80"></a>
+                </div>
+                <div class="span8">
+                    <a href="http://www.gurulink.ca">GuruLink Inc.</a> is a recruitment services and software company with offices in Toronto and Montreal that specializes in Information Technology.  GuruLink connects great talent with great opportunities.
                 </div>
             </div>
             <div class="row">
