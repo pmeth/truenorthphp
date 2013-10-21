33fd61256420a739bde857865ad49e65774552cc
Peter Meth <peter.meth@juicemobile.org>
2013-10-21 15:03:56 -0400

    adding jack milunsky to list of speakers and on schedule

--- schedule.php
+++ schedule.php
@@ -128,7 +128,9 @@
 		<tr class="schedule_slot"><th>13:00</th>
 			<td class="error">
 				<div class="talk_title">
-					TBD
+					<a class="fancybox fancybox.iframe" href="viewtalk.php?name=introduction_to_agile_&_scrum">Introduction to Agile &amp; Scrum</a>
+				</div>
+				<div class="talk_speaker">Jack Milunsky</div>
 				</div>
 			</td>
 			<td class="warning">
---
+++ speaker_data/Jack_Milunsky.php
@@ -0,0 +1,16 @@
+<?php
+
+$speakers[] = array (
+  'name' => 'Jack Milunsky',
+  'city' => 'Toronto, ON',
+  'country' => 'Canada',
+  'twitter' => '',
+  'image' => 'http://truenorthphp.ca/img/jack-milunsky.jpg',
+  'bio' => '',
+  'talks' => array (
+    array (
+      'title' => 'Introduction to Agile & Scrum',
+      'text' => '',
+    ),
+  ),
+);
