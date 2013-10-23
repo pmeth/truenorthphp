91018ae93b360662ad470f7ad9561596ab3957df
Peter Meth <peter.meth@juicemobile.org>
2013-10-23 14:17:20 -0400

    updating Jack Milunsky profile

--- speaker_data/Jack_Milunsky.php
+++ speaker_data/Jack_Milunsky.php
@@ -6,7 +6,11 @@ $speakers[] = array (
   'country' => 'Canada',
   'twitter' => '',
   'image' => 'http://truenorthphp.ca/img/jack-milunsky.jpg',
-  'bio' => '',
+	'bio' => 'A Senior Manager with 15+ years of experience building and leading product development teams in the creation, launch and promotion of industry leading, software products and services.
+
+As VP Technology at Juice Mobile, Jack heads up the product development team focused on building industry leading mobile advertising solutions connecting advertisers and publishers for effective management of mobile advertising campaigns. As an early adopter of Agile, Jack brings many years of experience transitioning teams to Scrum, optimizing product delivery and quality. Jack is one of only a few leaders with experience implementing Scrum at scale.
+
+Jack\'s experience spans Mobile, Web, Desktop and Enterprise Application development, in a wide range of companies from early stage startups to mature public companies and many industries including Health, Finance and Insurance ',
   'talks' => array (
     array (
       'title' => 'Introduction to Agile & Scrum',
