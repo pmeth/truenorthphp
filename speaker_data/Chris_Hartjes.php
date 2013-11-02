a0a3d396953c4576e47b1d76db67c39d46b3757c
Peter Meth <pmeth@rogers.com>
2013-11-02 11:57:24 -0400

    updating chris hartjes picture

--- speaker_data/Chris_Hartjes.php
+++ speaker_data/Chris_Hartjes.php
@@ -5,11 +5,11 @@ $speakers[] = array (
   'city' => 'Milton, ON',
   'country' => 'Canada',
   'twitter' => '',
-  'image' => 'http://www.littlehart.net/DRAW-ANYTHING-363.gif',
+  'image' => 'http://www.truenorthphp.ca/chris_hartjes.png',
   'bio' => 'Chris Hartjes has been building web applications of all shapes and sizes since 1998, ranging from catalogs for CD compilations for professional DJ’s to large-scale dating web sites.
-  
+
   A huge consumer of open-source software, Chris tries to give back to the community via his blog, by speaking at conferences, and by co-organizing TrueNorthPHP. He is also a big believer in the power of testing and automation as secret weapons for organizations to deliver high quality applications quickly.
-  
+
   He lives in Milton, Ontario, Canada with his long-suffering wife, two daughters, a furry pig masquerading as a cat, and the reincarnation of the Roman god of the underworld in feline form.',
   'talks' => array (
     array (
