1e5c113fae2084e8af55048e48baadcb1f603509
Peter Meth <peter.meth@juicemobile.org>
2013-11-05 12:51:01 -0500

    updating nick bio and talk description

--- speaker_data/Nick_Van_Weerdenburg.php
+++ speaker_data/Nick_Van_Weerdenburg.php
@@ -4,13 +4,13 @@ $speakers[] = array (
   'name' => 'Nick Van Weerdenburg',
   'city' => 'Toronto, ON',
   'country' => 'Canada',
-  'twitter' => '',
+  'twitter' => 'rangleio',
   'image' => 'http://www.gravatar.com/avatar/0038f44acaa72e3b7f4a1a7c2d077ae0?s=200&d=http://truenorthphp.ca/img/no-pic.jpg',
-  'bio' => '',
+  'bio' => 'Nick is the founder of rangle.io, a JavaScript consultancy specializing in AngularJS, Node.js, MongoDB and PhoneGap. He is also the organizer of the Toronto AngularJS and PhoneGap meetups. When not busy speaking about and teaching AngularJS, he leads the development of cutting edge AngularJS web and mobile apps and helps companies move to single page application architectures with AngularJS and RESTful JSON APIs.',
   'talks' => array (
     array (
-      'title' => 'Introduction to AngularJS',
-      'text' => 'This talk will introduce you to AngularJS',
+      'title' => 'AngularJS Fundamentals for Rapid HTML5 Development',
+      'text' => 'AngularJS is a hybrid HTML/JavaScript framework that drastically simplifies building web applications against RESTful web services. Join Nick Van Weerdenburg as he introduces AngularJS from the ground up in manner suitable for designers, developers, and anyone else with basic HTML 5 and JavaScript experience.',
     ),
   ),
 );
