19698c2c01c2c33dd6894595aaf55207de8ad60b
Jonathan Reinink <jonathan@reinink.ca>
2014-09-22 08:37:06 -0400

    Updated Jonathan Reinink's speaker info.
    Update city.
    Added Twitter username.
    Added slightly condensed bio.

--- speaker_data/Jonathan_Reinink.php
+++ speaker_data/Jonathan_Reinink.php
@@ -2,12 +2,11 @@
 
 $speakers[] = array (
   'name' => 'Jonathan Reinink',
-  'city' => 'Toronto, ON',
+  'city' => 'Beamsville, ON',
   'country' => 'Canada',
-  'twitter' => '',
+  'twitter' => 'reinink',
   'image' => 'http://www.gravatar.com/avatar/6074dbe5dfa361c9888a705f40a49af4?s=200&d=http://truenorthphp.ca/img/no-pic.jpg',
-  'bio' => 'Jonathan Reinink is long time (late 90\'s) front and back-end web developer with a keen interest in PHP. Jonathan currently serves as the Director of Web Development at a Canadian based marketing company, Compass Creative, where they provide branding and websites for construction companies. Jonathan spearheads their SaaS product, Worksites.net, a do-it-yourself website builder for contractors. After hours, Jonathan runs a second SaaS product, Church Social, a niche web-based management product for churches.
-In recent years Jonathan has transition from being a "lone wolf" developer to being much more active in the open-source community. Most notably, Jonathan has contributed Plates, a native PHP template system, which is part of The League of Extraordinary Packages. Jonathan has a strong design sense and is an advocate of using good design to help promote technical projects. Jonathan digs coding standards, and follows the PHP Framework Interop Group closely, and is currently assisting this organization with their marketing. Jonathan enjoys contributing to open-source projects, and has recently contributed a section on "Templating" to PHP: The Right Way.',
+  'bio' => 'Jonathan Reinink is a long time (late 90\'s) front and back-end web developer with a keen interest in PHP. Jonathan currently serves as the Director of Web Development at Compass Creative, a Canadian based marketing company that provides branding and websites to trades businesses. Jonathan spearheads their SaaS product, Worksites.net, a do-it-yourself website builder for contractors. After hours, Jonathan runs a second SaaS product, Church Social, a niche web-based management product for churches. In recent years Jonathan has transition from being a "lone wolf" developer to being much more active in the PHP community. Most notably, Jonathan has contributed Plates, a native PHP template system that\'s part of The PHP League. Jonathan has a strong design sense and is an advocate of using good design to help promote technical projects.',
   'talks' => array (
     array (
       'title' => 'Zero to The PHP League: The story of how Plates became a thing',
