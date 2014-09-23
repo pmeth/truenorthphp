41346cc236042111bb4314d296f9a85f777205c5
Peter Meth <peter.meth@achieve3000.com>
2014-09-22 21:08:14 -0400

    updating Dan Cousineau talk details

--- speaker_data/Daniel_Cousineau.php
+++ speaker_data/Daniel_Cousineau.php
@@ -10,10 +10,7 @@ $speakers[] = array (
   'talks' => array (
     array (
       'title' => 'Reframing The Problem: How To Think Like A Computer Scientist',
-      'text' => 'Often times we hit a wall and get stuck on a problem, only to realize much later and after lengthy and unnecessary pain the solution was simple if we just reframed the problem as something else. Thinking like a Computer Scientist involves being able to take a step back, abstracting what we know about a problem space, and realizing relations to other, solved, problems that were not immediately apparent. Together we\'ll tackle a problem, learn how to think, and come away with that "aha!" moment that we all know and love as developers. Originally a 10 minute lightning talk (with no code) that used up every second of that 10 minutes.
-I want to expand on the talk, do more background discussion, explain the actual solution, and provide code of the solution working in PHP.
-To give away the premise: it\'s about the algorithms used to solve an Eight Slider Puzzle (google the phrase if you\'re not sure what that is) that I learned in my AI course. Oddly enough, despite it seeming unrelated, the algorithm used to solve the puzzle is the same algorithm Google might use to find driving directions to the conference center.
-The focus is not the algorithm itself, but the thought process one uses to approach the problem and even figure out that these algorithms are relevant.',
+      'text' => 'Often times we hit a wall and get stuck on a problem, only to realize much later and after lengthy and unnecessary pain the solution was simple if we just reframed the problem as something else. Thinking like a Computer Scientist involves being able to take a step back, abstracting what we know about a problem space, and realizing relations to other, solved, problems that were not immediately apparent. Together we\'ll tackle a problem, learn how to think, and come away with that "aha!" moment that we all know and love as developers. Originally a 10 minute lightning talk (with no code) that used up every second of that 10 minutes.',
     ),
   ),
 );
