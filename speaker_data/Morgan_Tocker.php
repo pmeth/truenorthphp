c3e9ac131ab644bdc0c1a5cee213156b1172035d
Peter Meth <pmeth@delvia.com>
2015-09-16 10:56:10 -0400

    updating morgans bio

--- speaker_data/Morgan_Tocker.php
+++ speaker_data/Morgan_Tocker.php
@@ -6,7 +6,7 @@ $speakers[] = array (
   'country' => 'Canada',
   'twitter' => '',
   'image' => 'http://www.gravatar.com/avatar/02f5abe0b9204b736ea16103b02fa15e?s=200&d=http://truenorthphp.ca/img/no-pic.jpg',
-  'bio' => '',
+  'bio' => 'Morgan rejoined the MySQL team at Oracle in 2013 as MySQL Community Manager, having previously worked for MySQL and Sun Microsystems.  He blogs from <a href="http://tocker.ca">tocker.ca</a>, and especially likes the new features in MySQL that make operations life easier.',
   'talks' => array (
     array (
       'title' => 'MySQL 5.7 + JSON',
