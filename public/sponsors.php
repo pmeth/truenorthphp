7c82a552dcb383dbfb5ca40388111ac014260312
Peter Meth <pmeth@delvia.com>
2015-09-25 17:38:40 -0400

    adding a bunch of sponsor logos

--- public/sponsors.php
+++ public/sponsors.php
@@ -67,18 +67,14 @@ $page = 'sponsors';
                   <h3>Gold Level</h3>
                </div>
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="Company" src="http://www.dummyimage.com/220x58/000/fff&text=Your+Name+Here" width="220" height="58"></a>
+                  <a href="http://www.delvia.com/"><img alt="Delvia" src="/img/sponsors/delvia-logo.png" width="220" height="220"></a>
                </div>
                <div class="span8">
-                  Coming soon...
-               </div>
-            </div>
-            <div class="row" style="margin-top: 30px;">
-               <div class="thumbnail span3" style="margin-top: 44px;">
-                  <a href="http://www.truenorthphp.com/"><img alt="Le Company" src="http://www.dummyimage.com/210x82/000/fff&text=Your+Name+Here" width="210" height="82"></a>
-               </div>
-               <div class="span8">
-                  Coming soon...
+<a href="http://www.delvia.com">Delvia</a> started when several senior developers got to talking about our observations of the software development industry. We agreed that companies universally have issues hiring and keeping the right people. We also agreed that developers employed by large companies can have a tendency to not keep their skills up to date and thus have a difficult time adapting to a rapidly changing development landscape.<br>
+<br>
+As a result of this conversation we concluded that we should join together. Using our vast experience we could provide a better alternative and offer it to businesses of all sizes. We made a resolution to acquire the best talent, use the best tools and provide the necessary training to stay on top of the industry.<br>
+<br>
+Delvia is proud to support and serve the PHP community. We are interested in forming lasting relationships with developers of PHP and other fine languages.
                </div>
             </div>
             <div class="row">
@@ -92,7 +88,7 @@ $page = 'sponsors';
                   <a href="http://www.in2it.be/"><img alt="in2it" src="/img/sponsors/in2it-logo.png" width="210" height="149" /></a>
                </div>
                <div class="span8">
-                  In2it is the leading PHP consulting firm in the Benelux offering services to international enterprises, small to mid-sized businesses, governmental institutes, educational and non-profit organizations. We’re a market leader in consulting for PHP and Zend Framework application architectures, quality assurance on PHP projects and we provide training courses on-site and online.<br>
+                  <a href="http://www.in2it.be/">In2it</a> is the leading PHP consulting firm in the Benelux offering services to international enterprises, small to mid-sized businesses, governmental institutes, educational and non-profit organizations. We’re a market leader in consulting for PHP and Zend Framework application architectures, quality assurance on PHP projects and we provide training courses on-site and online.<br>
 <br>
 We are driven to face difficult challenges and turn them into success stories for our customers while coaching and training their development teams to operate at higher levels. This ensures knowledge stays inside the organisation and the whole team is more motivated.<br>
 <br>
@@ -101,18 +97,16 @@ We keep our engagement to the PHP community and do what we can to make things po
             </div>
             <div class="row" style="margin-top: 30px;">
                <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x210/000/fff&text=Your+Name+Here" width="210" height="210" /></a>
+                  <a href="http://www.jetbrains.com/"><img alt="JetBrains" src="/img/sponsors/jetbrains-logo.png" width="210" height="41" /></a>
                </div>
                <div class="span8">
-                  Coming soon...
-               </div>
-            </div>
-            <div class="row" style="margin-top: 30px;">
-               <div class="thumbnail span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x61/000/fff&text=Your+Name+Here" width="210" height="61" /></a>
-               </div>
-               <div class="span8">
-                  Coming soon...
+<a href="http://www.jetbrains.com/">JetBrains</a> is a software development company that's passionate about developer productivity. We design intelligent development tools to simplify your challenging tasks, automate the easy ones, and help you work with pleasure.<br>
+<br>
+PhpStorm is our PHP IDE that actually 'gets' your code. It supports PHP 5.3, 5.4, 5.5, 5.6 and 7 and offers on-the-fly error prevention, best autocompletion and code refactoring, zero configuration debugging, and a smart HTML, CSS, and JavaScript editor.<br>
+<br>
+We also provide top-notch IDEs for Java, JavaScript, Ruby, Python, and Objective-C, as well as a pack of tools for .NET developers and team productivity tools for continuous integration and issue tracking.<br>
+<br>
+With headquarters in Prague, Czech Republic, we also have R&amp;D labs in Russia, USA and Germany.
                </div>
             </div>
             <div class="row">
@@ -128,7 +122,7 @@ We keep our engagement to the PHP community and do what we can to make things po
                   <a href="http://www.pluralsight.com/"><img alt="Pluralsight" src="/img/sponsors/pluralsight-logo.png" width="220" height="18" /></a>
                </div>
                <div class="span8">
-                  Founded in 2004, Pluralsight is the global leader in online learning for professional software developers, IT specialists and creative technologists. As the world’s largest curated professional development platform for professionals, the company offers instant access to over 4,000 courses authored by top experts. With customers in more than 150 countries, Pluralsight serves as a career catalyst, delivering hands-on, practical training for the most in-demand and understaffed jobs of today. For more information, visit <a href="http://www.pluralsight.com">Pluralsight.com</a>.
+                  Founded in 2004, <a href="http://www.pluralsight.com/">Pluralsight</a> is the global leader in online learning for professional software developers, IT specialists and creative technologists. As the world’s largest curated professional development platform for professionals, the company offers instant access to over 4,000 courses authored by top experts. With customers in more than 150 countries, Pluralsight serves as a career catalyst, delivering hands-on, practical training for the most in-demand and understaffed jobs of today. For more information, visit <a href="http://www.pluralsight.com">Pluralsight.com</a>.
                 </div>
             </div>
             <br>
@@ -137,7 +131,7 @@ We keep our engagement to the PHP community and do what we can to make things po
                   <a href="http://www.mobilexco.com/"><img alt="The Mobile Experience Company" src="/img/sponsors/mobilexco-logo.png" width="210" height="49" /></a>
                </div>
                <div class="span8">
-                    MobileXCo - "The Mobile Experience Company" is made up of specialists with over 50 years of digital, marketing, social and mobile expertise. While we have a variety of backgrounds and experiences, we share a common belief.<br><br>
+                    <a href="http://www.mobilexco.com/">MobileXCo - "The Mobile Experience Company"</a> is made up of specialists with over 50 years of digital, marketing, social and mobile expertise. While we have a variety of backgrounds and experiences, we share a common belief.<br><br>
                     "Marketing is about relationships. And relationships begin and end with people. People like you and me."<br><br>
                     This is the guiding force behind everything we do – from the development of our technology, the data we capture, to the campaigns we execute.
                 </div>
@@ -149,29 +143,22 @@ We keep our engagement to the PHP community and do what we can to make things po
                <div class="span12">
                   <h3>Community Level</h3>
                </div>
+               <div class="span3">
+                  <a href="https://voicesoftheelephpant.com/"><img alt="Voices of the ElePHPant" src="/img/sponsors/voices-of-the-elephpant-logo.png" width="180" height="180"></a>
+               </div>
                <div class="span2" style="padding-top: 10px;">
                   <a href="http://gtaphp.org/"><img alt="GTA PHP Meetup Group" src="/img/sponsors/gtaphp-logo.jpg" width="120" height="120" /></a>
                </div>
-               <div class="span2">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/110x165/000/fff&text=Your+Name+Here" width="110" height="165" /></a>
-               </div>
-               <div class="span2">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/165x165/000/fff&text=Your+Name+Here" width="165" height="165" /></a>
-               </div>
                <div class="span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x73/000/fff&text=Your+Name+Here" width="210" height="73" /></a>
+                  <a href="http://www.daycamp4developers.com/"><img alt="Day Camp 4 Developers" src="/img/sponsors/daycamp-4-developers-logo.png" width="210" height="91" /></a>
                   <br>
                   <br>
                   <br>
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/210x51/000/fff&text=Your+Name+Here" width="210" height="51"></a>
+                  <br>
+                  <a href="http://www.oracle.com/"><img alt="Oracle" src="/img/sponsors/oracle-logo.png" width="210" height="27"></a>
                </div>
                <div class="span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/180x180/000/fff&text=Your+Name+Here" width="180" height="180"></a>
-               </div>
-            </div>
-            <div class="row">
-               <div class="span3">
-                  <a href="http://www.example.com/"><img alt="A Sponsor" src="http://www.dummyimage.com/205x124/000/fff&text=Your+Name+Here" width="205" height="124"></a>
+                  <a href="https://nomadphp.com/"><img alt="Nomad PHP" src="/img/sponsors/nomad-php-logo.png" width="180" height="180"></a>
                </div>
             </div>
          </div>
