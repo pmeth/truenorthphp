f2da6685737da4549b8320bb6c2fd92429abbee0
Peter Meth <pmeth@delvia.com>
2016-10-06 13:06:32 -0400

    updating ilia talk abstracts

--- speaker_data/Ilia_Alshanetsky.php
+++ speaker_data/Ilia_Alshanetsky.php
@@ -10,11 +10,11 @@ $speakers[] = array (
   'talks' => array (
     array (
       'title' => 'Business Logic Security',
-      'text' => '',
+      'text' => 'This session will outline security practices and solutions designed to address security issues within application\'s business and processing logic, which are often overlooked in favour of basic security flaws such as SQL Injection, XSS, etc... The session will focus on how to implement mechanisms for improving data access, avoiding common ACL pitfalls, etc.',
     ),
     array(
         'title' => 'Deep Dive Into Browser Performance',
-        'text' => '',
+        'text' => 'In the world of the Internet user\'s experience is in many cases controlled by the browser, and the browser\'s ability to render the page is the ultimate measure of how fast or slow a particular page and/or application is in the eyes of the user. This session will outline the tools that can be used to effectively measure the user experience in the browser as well as outline a number of approaches and performance tricks designed to improve and accelerate that experience.',
     )
   ),
 );
